057951b5585654ac24cd01b4a0603cd5d44c4e8c
Kathy <kathy@cater2.me>
2013-02-07 13:30:23 -0500

    google voice is tested and working yayyy

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,5 +1,6 @@
 require 'googlevoiceapi'
 require 'google_drive'
+require 'yaml'
 
 APP_CONFIG = YAML::load_file("config.yml")
 
@@ -16,7 +17,7 @@ task :message_vendors do
 	message = ""
 	row_data = []
 	# get array of all numbers and vendor names
-	for row in 2..@doc.num_rows
+	for row in 2..@doc.num_rows/
 		row_data << [@doc[row, 2] != "" ? clean_numbers(@doc[row, 2]) : clean_numbers(@doc[row, 6]), @doc[row, 3]]
 	end
 
@@ -28,29 +29,27 @@ task :message_vendors do
 		vendor_name = r[1]
 		message = Vendor.find_by_name(vendor_name).first.get_message
 
-		# correct grammar for number of confirmed orders]
-
-
-		# use google voice to send sms
+    # use google voice to send sms
 		@api.sms(number, message)
+    p "Texted #{number}"
 
 		# write "Awaiting Response" into Status column in @doc if sms is successful
 		for row in 2..@doc.num_rows
-			if @doc[row, 2] == r[0] || @doc[row, 6] == r[0]
+			if clean_numbers(@doc[row, 2]) == r[0] or clean_numbers(@doc[row, 6]) == r[0]
 				@doc[row, 4] = "Awaiting Response"
-			end
+        p "Updated Status"
+      end
+      @doc.save()
 		end
 	end
 
 end
 
 task :reset_status_for_testing do
-
 	for row in 2..@doc.num_rows
 		@doc[row, 4] = "Confirmed"
 		@doc.save()
 	end
-
 end
 
 
