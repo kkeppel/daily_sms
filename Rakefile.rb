0d59fb35fa1c32840a9dd1344577466086db80b4
Kathy <kathykeppel@gmail.com>
2013-03-18 20:50:28 -0400

    added task to test spreadsheet creation --- it works yayyyy

--- Rakefile.rb
+++ Rakefile.rb
@@ -8,7 +8,7 @@ $LOAD_PATH << File.dirname(__FILE__)
 APP_CONFIG = YAML::load_file("config.yml")
 
 
-#Creadentials
+#Credentials
 login = APP_CONFIG["google_login"]
 pass =  APP_CONFIG["google_password"]
 email_user = APP_CONFIG["email_user"]
@@ -16,14 +16,14 @@ email_pass = APP_CONFIG["email_pass"]
 
 require 'models/init'
 
-# Spreasheet Setup
-session = GoogleDrive.login(login, pass)
-@doc = session.spreadsheet_by_title("Daily Order Confirmations #{Date.today.month}/#{Date.today.day}").worksheets[0]
+# Spreadsheet Setup
+@session = GoogleDrive.login(login, pass)
+# @doc = @session.spreadsheet_by_title("Daily Order Confirmations #{Date.today.month}/#{Date.today.day}").worksheets[0]
 
 # Google Voice Setup
 @api = GoogleVoice::Api.new(login, pass)
 
-# Maiil Setup
+# Mail Setup
 options = { :address              => "smtp.gmail.com",
             :port                 => 587,
             :domain								=> "cater2.me",
@@ -36,10 +36,27 @@ Mail.defaults do
   delivery_method :smtp, options
 end
 
+task :test_spreadsheet_creation do
+	# create spreadsheet for today
+	new_spreadsheet = @session.create_spreadsheet("Daily Order Confirmations #{Date.today.month}/#{Date.today.day}")
+	file = @session.file_by_title(new_spreadsheet.title)
+	file.acl.push(scope_type: "user", scope: "kathykeppel@gmail.com", role: "writer")
+	new_spreadsheet = new_spreadsheet.worksheets[0]
+	new_spreadsheet[1, 1] = "Vendor Name"
+	new_spreadsheet[1, 2] = "Text Number"
+	new_spreadsheet[1, 3] = "Order for Time"
+	new_spreadsheet[1, 4] = "Client Name"
+	new_spreadsheet[1, 5] = "Status"
+	new_spreadsheet.save()
+end
+
 task :message_vendors do
 	row_data = []
 	succeded = []
 	failed = []
+
+	
+
 	# get array of all numbers and vendor names
 	for row in 2..@doc.num_rows
 		if @doc[row,1].downcase.match(/text/) #TODO: Change to text
@@ -74,7 +91,8 @@ task :message_vendors do
 	content = ["Succeeded:",succeded.join("\r\n"),"Failed:", failed.join("\r\n")].join("\n")
 
 	Mail.deliver do
-	  to 'yuriy@cater2.me'
+	  # to 'yuriy@cater2.me'
+	  to 'kathy@cater2.me'
 	  from login
 	  subject subject
 	  body content
